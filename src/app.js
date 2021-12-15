ca1fda5170f30c23c3fc9e56bbbfda795b1a074a
Itay Leybovich <itayleybovich@gmail.com>
2021-12-15 15:00:22 +0200

    added cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const requestsRouter = require('./routers/requests.router');
+const cors = require('cors');
+
+const corsOptions = {
+    origin: "*",
+    methods: ['GET'],
+}
 
 const app = express();
 app.use(requestsRouter);
+app.use(cors(corsOptions));
 
 module.exports = app;
